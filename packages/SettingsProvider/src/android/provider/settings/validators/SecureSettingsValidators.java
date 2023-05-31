68400e8132db0434427d9112f34ef97a2081d27f
someone5678 <nemui3353@gmail.com>
2023-05-31 22:33:20 +0900

    base: Hide ADB and developer setting enable status [1/2]
    * Some apps check whether ADB or developer setting is enabled
    * Return 0 to selected apps when those apps getInt
      ADB_ENABLED, ADB_WIFI_ENABLED, DEVELOPMENT_SETTINGS_ENABLED
    * Code is adapted and stripped down from cutout force full screen
    
    Ref:
    https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    yaap/frameworks_base@232260f
    
    Squashed with:
    
        base: HideDevStatus: move to getIntForUser to cover more scenarios
    
        * Adapt to current project as we want to set status individually per-app
          for case apps require ADB or Dev settings for their functionality
    
        e.g. SystemUI Tuner by Zachary Wander
        https://play.google.com/store/apps/details?id=com.zacharee1.systemuituner&hl=en_US&gl=US
    
        Ref:
        https://github.com/RisingTechOSS/android_frameworks_base/commit/77be7be42060dc5119c6dccef874c9a439aed210
    
        base: HideDeveloperStatus: Query all apps installed by any user [1/2]
    
        * And set value for all user
    
        Change-Id: Ibd2a5dfab832e6bdc6d074ce41401f4905602152
    
        HideDeveloperStatusUtils: Avoid NPE with mApps
    
        * https://bin.mkr.pw/~5785442c632ce757b6267756.
    
        Change-Id: I3c35a6ede3cd5ce334de9e3267932440143b421e
        Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    
    Change-Id: Ibbf92c7b1f1181d2508cb22ff2fc2017f9e59706
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -462,5 +462,6 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.MANDATORY_BIOMETRICS_REQUIREMENTS_SATISFIED,
                 new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.HIDE_DEVELOPER_STATUS, ANY_STRING_VALIDATOR);
     }
 }
