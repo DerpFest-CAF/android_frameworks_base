83eb7c5797aab4412889dc1712ac0aaf788783d6
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2024-12-09 08:16:41 +0530

    base: Add Volume Panel action[1/3]
    Change-Id: Ie35051556d2d4be0fdfb2c82963561bc976b46ce
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2325,6 +2325,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KILL_APP:
                 ActionUtils.killForegroundApp(mContext, mCurrentUserId);
                 break;
+            case VOLUME_PANEL:
+                ActionUtils.toggleVolumePanel(mContext);
             default:
                 break;
         }
