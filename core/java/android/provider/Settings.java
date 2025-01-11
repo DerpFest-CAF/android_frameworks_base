25ad120ee79492aec8510334da9bb8c1b76c7f9e
NurKeinNeid <mralexman3000@gmail.com>
2025-01-11 01:11:55 +0100

    secure

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12923,6 +12923,13 @@ public final class Settings {
         public static final String VPN_ENFORCE_DNS_STORE = "vpn_enforce_dns_store";
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
